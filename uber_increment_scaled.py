0c88e0f2105eec382b6e7f01d14d3141d6ec7ccb
Nick <nicholaswood89@gmail.com>
2020-02-18 18:25:28 -0500

    added note on scaled answer

--- uber_increment_scaled.py
+++ uber_increment_scaled.py
@@ -9,9 +9,9 @@ def increment(num_list):
                 counter -= 1
             elif num_list[counter] > num_list[counter-1]:
                 x = num_list[counter-1] #the first num to dec from prev starting at end
-                temp_list = sorted(num_list[counter-1:]) #sorting the list from the 1st dec num till end 
+                temp_list = sorted(num_list[counter-1:]) #sorting the list from the 1st dec num till end
+                # to optimize it further get rid of the use of sort
                 num_x = temp_list.count(x) #count of x (first num to dec)
-                # next_num_x = temp_list[temp_list.index(x) + num_x] #finding the next higher number
                 beg = num_list[:counter-1]
                 y = [temp_list.pop(temp_list.index(x) + num_x)]
                 #temp list is end
@@ -20,6 +20,7 @@ def increment(num_list):
 
 # print(increment([9,1,5,8,7,6,5,4,3,2,1]))
 # print(increment([1,2]))
+# print(increment([9,2,6,4]))
 
 def recursive_increment(num_list):
     lim = len(num_list) * -1
@@ -44,7 +45,7 @@ def recursive_increment(num_list):
 
 # recursive_increment([1,2])
 
-recursive_increment([1,2,3,4])
+# recursive_increment([1,2,3,4])
 
 
 
